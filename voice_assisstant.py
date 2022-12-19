f7c1182b412195f7be2bfdfd7d9624410e48e56e
Joseph DellaMorte <joseph@delresearch.com>
2022-12-19 17:03:07 -0500

    added necessary trailing commas to tuples

--- voice_assisstant.py
+++ voice_assisstant.py
@@ -5,32 +5,64 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from gtts import gTTS
 import sys
+import datetime
 
 class voice_assistant():
     def __init__(self):
-        pass
+        self.lower_text='no text yet'
+        self.inquisitions_response_map= {
+                ("how many days are in a week","there are seven days in a week right?"):"there are seven days in a week unless you are the beatles",
+                ("how long does it take to make a christmas tree grow",):"six to eight years",
+                ("hey part keeper","hey park keeper",):"I'm sorry, part keeper is down",
+                ("how much wood could a wood chuck chuck if a wood chuck could chuck wood?",):"depending on age and size, about a cord and a half",
+                ("what is the time",):"I don't know that yet",
+                ("what is the year",):"I don't know that yet",
+                ("when was iggy born",):"July 28 2022",
+                }
+        self.next_response=None
+
+    def score_phrase(self,text):
+        return fuzz.partial_ratio(text,self.lower_text)
+
+    def best_response(self):
+        best_score=0
+        for inquisition_list in self.inquisitions_response_map.keys():
+            for question in inquisition_list:
+                score=self.score_phrase(question)
+                print(str(question) + " " + str(score))
+                if (score > best_score) and score > 90:
+                    best_score=score
+                    print('better score '+ str(score))
+                    self.next_response=self.inquisitions_response_map[inquisition_list]
+                
+
 
     def aprint(self,text,language='en',slow_speed=False):
+        '''Given a string the computer reads off the text'''
         myobj = gTTS(text=text, lang=language, slow=slow_speed)
         myobj.save("acoustic_print.mp3")
         os.system("afplay acoustic_print.mp3")
 
     def record_first(self,durration):
+        '''records to the first file'''
         #kick off decoding of second audio
         os.system('ffmpeg -y -f avfoundation -i ":0" -t '+str(durration)+' first_audio.mp3 &>/dev/null')
 
         
     def record_second(self,durration):
+        '''records to the second file'''
         #kick off decoding of first record_audio
         #start recording of second audio
         os.system('ffmpeg -y -f avfoundation -i ":0" -t '+str(durration)+' second_audio.mp3 &>/dev/null')
 
     def record_third(self,durration):
+        '''records to the third file'''
         #kick off decoding of second record_audio
         #start recording of third audio
         os.system('ffmpeg -y -f avfoundation -i ":0" -t '+str(durration)+' third_audio.mp3 &>/dev/null')
 
     def process_file(self,file):
+        '''runs the audio recording through the whisper speech recognition library'''
 
         # load audio and pad/trim it to fit 30 seconds
         audio = whisper.load_audio(file)
@@ -39,55 +71,26 @@ class voice_assistant():
         # make log-Mel spectrogram and move to the same device as the model
         mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-        '''
-        # detect the spoken language
-        _, probs = model.detect_language(mel)
-        #print(f"Detected language: {max(probs, key=probs.get)}")
-        ''' 
-
         # decode the audio
         options = whisper.DecodingOptions(fp16=False, language=False)
-        result = whisper.decode(model, mel, options)
+        self.result = whisper.decode(model, mel, options)
 
         # print the recognized text
-        print(result.text)
-        lower_text=str.lower(result.text)
-        #if "Sarah" in result.text:
-        #    print("\a\a\a\a\a\a\a\a\a\a")
-        hpk=fuzz.partial_ratio("hey part keeper",lower_text)
-        hppk=fuzz.partial_ratio("hey park keeper",lower_text)
-        hmmsdwh=fuzz.partial_ratio("how many minislims do we have?",lower_text)
-        archibald=fuzz.partial_ratio("how much wood could a wood chuck chuck if a wood chuck could chuck wood?",lower_text)
+        print(self.result.text)
+        self.lower_text=str.lower(self.result.text)
+        #print(self.lower_text)
         if verbosity:
+            pass
+            '''
             print("'hey part keeper' partial match score:" + str(hpk))
             print("'hey park keeper' partial match score:" + str(hppk))
             print("'how many minislims do we have?' partial match score:" + str(hmmsdwh))
-
-        if (hpk > 90) | (hppk > 90):
-            helper.aprint('hey user! whats up?')
-        elif (hmmsdwh> 90):
-            helper.aprint("I'm not sure but in the future I might be able to tell you")
-        elif (archibald > 90):
-            helper.aprint("Depending on age and size, about a quart and a half.")
-
-        def match_phrase(self):
-            phrases=[ "can you check how many items are in _____",
-                     "could you check the stock on the minislims",
-                     "how many modems do we have in _____",
-                     "what is located in location _____",
-                     "what do we have in location ______",
-                     "what are the names of the things in location _____",
-                     "what is in this box?",
-                     "what's the quantity of male microfit pins?",
-                     "how many female minifit pins do we have?",
-                     "what do you call the part in this bin",
-                     "add a part to location _____",
-                     "could you add a part to location ______",
-                     "could you remove a piece from the tray",
-                     "please take ##### out of the male minifit pins stock",
-                    ]
-
-            '''(nicety,verb,)'''
+            '''
+        self.best_response()
+        if (self.next_response!=None):
+            print('TALKING')
+            helper.aprint(self.next_response)
+            self.next_response!=None
 
         
 if __name__ == "__main__":
